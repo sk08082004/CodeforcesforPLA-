1adb933e5906c6179c2c67d007aa7ff0aa732ac6
Sunny Kumar <sk08082004@gmail.com>
2026-09-08 21:04:41 +0530

    .java

---
+++ Merge Two Sorted Linked Lists.java
@@ -0,0 +1,41 @@
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+
+        ListNode temp1 = list1;
+        ListNode temp2 = list2;
+        ListNode newList = new ListNode(0);
+        ListNode head = newList;
+
+        while (temp1 != null && temp2 != null)
+        {
+            if (temp1.val < temp2.val)
+            {
+                newList.next = temp1;
+                temp1 = temp1.next;
+            }
+            else
+            {
+                newList.next = temp2;
+                temp2 = temp2.next;
+            }
+
+            newList = newList.next;
+        }
+
+        while (temp1 != null)
+        {
+            newList.next = temp1;
+            temp1 = temp1.next;
+            newList = newList.next;
+        }
+
+        while (temp2 != null)
+        {
+            newList.next = temp2;
+            temp2 = temp2.next;
+            newList = newList.next;
+        }
+
+        return head.next;
+    }
+}
